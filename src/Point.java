e033b7174caad92e754d3516ab73ad230ff46d39
jdvalera <johnlemuelvalera@yahoo.com>
2018-01-03 01:05:44 -0800

    Confirmed that slopeOrder is working. Added better testing values to confirm.

--- src/Point.java
+++ src/Point.java
@@ -99,7 +99,7 @@ public class Point implements Comparable<Point> {
     
     private class SlopeOrder implements Comparator<Point> {
 		@Override
-		public int compare(Point p1, Point p2) {/*
+		public int compare(Point p1, Point p2) {
 			Double s1 = 0.0 , s2 = 0.0;
 			if (x == p1.x && y == p1.y)
 				s1 = Double.NEGATIVE_INFINITY;
@@ -125,14 +125,15 @@ public class Point implements Comparable<Point> {
 				return 1;
 			else
 				return 0;
-			*/
 			
+			/*
 			if (Point.this.slopeTo(p1) < Point.this.slopeTo(p2))
 				return -1;
 			else if (Point.this.slopeTo(p1) > Point.this.slopeTo(p2))
 				return 1;
 			else
 				return 0;
+				*/
 		}
     }
 
@@ -165,11 +166,13 @@ public class Point implements Comparable<Point> {
      */
     public static void main(String[] args) {
         /* YOUR CODE HERE */
-    	Point p1 = new Point(1,2);
-    	Point p2 = new Point(1,2);
-    	Point p3 = new Point(1,4);
-    	Point p4 = new Point(2,2);
+    	Point p1 = new Point(1,1);
+    	Point p2 = new Point(1,1);
+    	Point p3 = new Point(1,2);
+    	Point p4 = new Point(2,1);
     	Point p5 = new Point(5,10);
+    	Point p6 = new Point(2,2);
+    	Point p7 = new Point(2,4);
     	
     	System.out.println("Degenerate line: " + p1.slopeTo(p2));
     	System.out.println("Vertical line: " + p1.slopeTo(p3));
@@ -182,19 +185,27 @@ public class Point implements Comparable<Point> {
     	System.out.println("Same y value smaller x: " + p1.compareTo(p4));
     	System.out.println("Same point values: " + p1.compareTo(p2));
     	
-    	Point[] A = new Point[3];
+    	Point[] A = new Point[7];
     	A[0] = p1;
-    	A[1] = p5;
+    	A[1] = p2;
     	A[2] = p3;
+    	A[3] = p4;
+    	A[4] = p5;
+    	A[5] = p6;
+    	A[6] = p7;
+    	
+    	for (int i = 0; i < A.length; i++) {
+    		System.out.println("Slope to A[" + i + "]: " + A[0].slopeTo(A[i]));
+    	}
     	
     	for (Point a : A) {
-    		System.out.println(a);
+    		System.out.println("Before Sort: " + a);
     	}
     	
     	Arrays.sort(A, p2.slopeOrder());
     	
     	for (Point a : A) {
-    		System.out.println(a);
+    		System.out.println("After Sort: " + a);
     	}
 
     	
