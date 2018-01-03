2113c02d41810f524414af8ad31484e52de2757e
jdvalera <johnlemuelvalera@yahoo.com>
2018-01-03 01:50:32 -0800

    Added private ArrayList instance variable. Started on constructor loop

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,11 +1,23 @@
+import java.util.ArrayList;
 
 public class BruteCollinearPoints {
 	
 	LineSegment[] lineSegments = null;
+	private ArrayList<LineSegment> tempSegments = null;
 	
 	// finds all line segments containing 4 points
 	public BruteCollinearPoints(Point[] points) {
-	
+		tempSegments = new ArrayList<LineSegment>();
+		
+		for(int i = 0; i < points.length; i++) {
+			for (int j = i+1; j < points.length; j++) {
+				for (int k = j+1; k < points.length; k++) {
+					for (int l = k+1; l < points.length; l++) {
+						
+					}
+				}
+			}
+		}
 	}
 	
 	// the number of line segments
