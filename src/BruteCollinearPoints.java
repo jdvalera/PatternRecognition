399312efdb282128353b613dd5e8d73fdd6e15f9
jdvalera <johnlemuelvalera@yahoo.com>
2018-01-03 01:30:54 -0800

    Added new class BruteCollinearPoints

---
+++ src/BruteCollinearPoints.java
@@ -0,0 +1,25 @@
+
+public class BruteCollinearPoints {
+	
+	LineSegment[] lineSegments = null;
+	
+	// finds all line segments containing 4 points
+	public BruteCollinearPoints(Point[] points) {
+	
+	}
+	
+	// the number of line segments
+	public int numberOfSegments() {
+		return 0;
+	}
+	
+	// the line segments
+	public LineSegment[] segments() {
+		return lineSegments;	
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
