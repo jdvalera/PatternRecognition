3667ef674aa25452e136554c05465c145c7b2e41
jdvalera <johnlemuelvalera@yahoo.com>
2018-01-04 16:36:23 -0800

    Added testing code and finished segments method

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -33,11 +33,30 @@ public class BruteCollinearPoints {
 	
 	// the line segments
 	public LineSegment[] segments() {
+		lineSegments = new LineSegment[tempSegments.size()];
+		lineSegments = tempSegments.toArray(lineSegments);
 		return lineSegments;	
 	}
 
 	public static void main(String[] args) {
-
+		Point p1 = new Point(1,1);
+		Point p2 = new Point(2,2);
+		Point p3 = new Point(3,3);
+		Point p4 = new Point(4,4);
+		
+		Point[] points = new Point[4];
+		points[0] = p1;
+		points[1] = p2;
+		points[2] = p3;
+		points[3] = p4;
+		
+		BruteCollinearPoints b = new BruteCollinearPoints(points);
+		System.out.println(b.numOfSegments);
+		LineSegment[] lines;
+		lines = b.segments();
+		for(LineSegment l : lines) {
+			System.out.println(l);
+		}
 	}
 
 }
